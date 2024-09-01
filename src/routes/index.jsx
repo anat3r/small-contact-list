0545406b1af93107655eb1639edce476a255f64b
Anat3r <ruslanbedychevwork@gmail.com>
2024-09-01 10:35:00 +0200

    fix colours

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,11 @@
 export default function Index() {
   return (
     <p id="zero-state">
-      This is a demo for React Router.
+      This is a demo project.
       <br />
       Check out{" "}
-      <a href="https://reactrouter.com">
-        the docs at reactrouter.com
+      <a href="https://github.com/anat3r/small-contact-list/">
+        the project github page
       </a>
       .
     </p>
